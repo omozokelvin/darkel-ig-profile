b509e26955ee8b1d558e4cd58c14fbfaf572fb1e
Kelvin <kelvinjune2@chaka.ng>
2022-01-31 23:46:05 +0100

    First commit after writing get instagram info

---
+++ src/routers/profile.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const getInstagramProfile = require('../utils/getInstagramProfile');
+
+const router = new express.Router();
+
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const userInfo = await getInstagramProfile(username);
+
+    res.send({
+      status: 200,
+      data: userInfo,
+      success: true,
+      message: 'Successful',
+    });
+  } catch (error) {
+    res.status(404).send();
+  }
+});
+
+module.exports = router;
---
+++ src/utils/getInstagramProfile.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+
+const getInstagramProfile = async (username) => {
+  try {
+    const urlToRead = `https://www.instagram.com/${username}/`;
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.goto(urlToRead);
+
+    const userInfo = await page.evaluate(() => {
+      const profileImage = document.querySelector('img[alt~="profile"]');
+      const h1 = document.querySelector('h1');
+      const followers = document.querySelector('ul>li:nth-child(2)>a>span');
+      const following = document.querySelector(
+        'ul > li:nth-child(3) > a > span'
+      );
+      const website = document.querySelector(
+        'section > main > div > header > section > div > a'
+      );
+
+      return {
+        profileImage: profileImage?.src || '',
+        name: h1?.innerText || '',
+        followers: followers?.innerText || '',
+        following: following?.innerText || '',
+        website: website?.innerText || '',
+      };
+    });
+
+    browser.close();
+
+    return userInfo;
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports = getInstagramProfile;
